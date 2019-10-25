6aa4ac9828cf90479ab6f87f04671141b4b4f490
EdmanWang <2569320889@qq.com>
2019-10-25 17:57:43 +0800

    create project

---
+++ src/main/java/com/wangguoxiong/java/concurrency/chapter1/ThreadCreate1.java
@@ -0,0 +1,23 @@
+package com.wangguoxiong.java.concurrency.chapter1;
+
+public class ThreadCreate1 extends Thread {
+
+    @Override
+    public void run() {
+       while (true){
+
+       }
+    }
+
+    public static void main(String[] args) {
+        ThreadCreate1 threadCreate1 = new ThreadCreate1();
+        threadCreate1.start();
+
+        try {
+            Thread.sleep(25_000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("main thread finish done");
+    }
+}
