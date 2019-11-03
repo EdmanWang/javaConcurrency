d594127455a5178852f7a185967cc5c877cffc12
EdmanWang <2569320889@qq.com>
2019-11-03 22:49:44 +0800

    update

--- src/main/java/com/wangguoxiong/java/concurrency/chapter8/ReadWriteLockTest.java
+++ src/main/java/com/wangguoxiong/java/concurrency/chapter8/ReadWriteLockTest.java
@@ -3,7 +3,7 @@ package com.wangguoxiong.java.concurrency.chapter8;
 public class ReadWriteLockTest {
 
     public static void main(String[] args) {
-        ShareData shareData = new ShareData(10);
+        final ShareData shareData = new ShareData(10);
         new ReadWorker(shareData).start();
         new ReadWorker(shareData).start();
         new ReadWorker(shareData).start();
